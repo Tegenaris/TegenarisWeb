d0d8100bf55af3fbd923c6d4be1c7f372c0b2709
Marcin Wasko <tegenarisweb@gmail.com>
2021-02-02 15:00:36 +0100

    Restructure

--- Shophelper/Old_API/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include_once('AutoLoader.php');
-    
-    // Register the directory to your include files
-    AutoLoader::registerDirectory('tests');
-?>
--- Shophelper/Old_API/src/triangle.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Triangle {
-    function CalculateTriangle($a, $h){
-            $ret = 1/2*$a*$h;
-            return $ret;
-    }
-}
-?>
