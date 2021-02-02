81422c6c094bba277607874258d2dfebe80469d9
Marcin Wasko <tegenarisweb@gmail.com>
2021-02-02 14:55:34 +0100

    Restructure

---
+++ Shophelper/API/Old_API/src/rsc/dbCheck.php
@@ -0,0 +1,28 @@
+<?php
+	#Connect to localhost MySQL
+	include('APIResources/environmentalConfig.php');
+
+	if ($ignoreServer){
+		define('host', 'localhost'); //usually localhost  
+		define('username', 'Jez'); //your username assigned to your database  
+		define('password', 'Sicarius147'); //your password assigned to your user & database
+		define('sb_database', 'ShopHelper'); //your database name
+	}
+	else{
+		define('host', 'mysql.cba.pl'); //usually localhost  
+		define('username', 'jezmarcin'); //your username assigned to your database  
+		define('password', 'ZYwavx3dh48tUI'); //your password assigned to your user & database
+		define('sb_database', 'jezmarcin_cba_pl'); //your database name
+	}
+	function connect_MySQL(){
+		try{
+			$pdo = new PDO('mysql:host='.host.';dbname='.sb_database.';encoding=utf8',username,password);
+			return $pdo;
+		}
+		catch(PDOException $e){
+			echo('Brak połączenia z serwerem');
+			return false;
+		}
+	}
+?>
+
