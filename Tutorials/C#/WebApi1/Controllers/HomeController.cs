04ec51bb79121d524070ef8525712300cc4eefcf
Marcin Wasko <marcin.j.wasko@gsk.com>
2022-04-04 01:46:09 +0200

    Added the API controller

--- Tutorials/C#/WebApi1/Controllers/HomeController.cs
+++ Tutorials/C#/WebApi1/Controllers/HomeController.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace WebApi1.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ApiController
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
 
-            return View();
         }
     }
 }
